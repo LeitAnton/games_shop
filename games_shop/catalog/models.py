3e6b6c79be36e61f2a5003b6560664e572c16ab8
LeitAnton <anton_lobanov02@mail.ru>
2022-10-21 11:54:05 +0300

    Created models (Category nad Product)

--- games_shop/catalog/models.py
+++ games_shop/catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name="Category")
+    data_release = models.DateTimeField()
+    pub_date = models.DateTimeField(auto_now=True)
