0b3eb8c51696fac1bcc2eec0d0dd68132fba2592
LeitAnton <anton_lobanov02@mail.ru>
2022-10-21 12:12:28 +0300

    Created output in admin panel

--- games_shop/catalog/admin.py
+++ games_shop/catalog/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = (
+        "id",
+        "title",
+    )
+    list_editable = (
+        "title",
+    )
+    search_fields = (
+        "title",
+        "description",
+    )
+    prepopulated_fields = {
+        "slug":(
+            "title",
+        )
+    }
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+
+    list_display = (
+        "id",
+        "title",
+        "price",
+        "category",
+        "date_release",
+        "date_publish",
+    )
+    list_filter = (
+        "price",
+        "category",
+        "date_release",
+        "date_publish",
+    )
+    list_editable = (
+        "title",
+        "slug",
+        "price",
+        "date_publish",
+        "date_release",
+    )
+    search_fields = (
+        "title",
+        "description",
+        "price",
+        "date_release",
+    )
+    prepopulated_fields = {
+        "slug":(
+            "title",
+        )
+    }
+    date_hierarchy = "date_publish"
