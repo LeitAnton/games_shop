ed2ac04dceb7409fb298dd587d6e581d6e10aa31
LeitAnton <anton_lobanov02@mail.ru>
2022-10-22 12:52:13 +0300

    Create categories page and catalog page

--- games_shop/users/views.py
+++ games_shop/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import CreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class SignUp(CreateView):
+    model = User
+    form_class = CreationForm
+    success_url = reverse_lazy("signup")
+    template_name = "signup.html"
