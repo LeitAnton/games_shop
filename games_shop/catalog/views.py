ede1f90ae6f8b00bbf62fdd8465a51e49f76c65e
LeitAnton <anton_lobanov02@mail.ru>
2022-10-22 17:34:41 +0300

    Work with visualization

--- games_shop/catalog/views.py
+++ games_shop/catalog/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import models
+from .models import Product, Category
+from django.core.paginator import Paginator
 
-# Create your views here.
+
+def catalog(request):
+    products_list = Product.objects.all()
+    paginator = Paginator(products_list, 3)
+    return render(request, "catalog.html", {"products" : })
 
 
-def index(request):
+def categories(request):
     return render(request, "categories.html")
+
+
+def cart(request):
+    return redirect('catalog.html')
