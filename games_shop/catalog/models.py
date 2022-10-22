7b8c633ac1ccbbcf59217483386ef9d080ce93d9
LeitAnton <anton_lobanov02@mail.ru>
2022-10-22 17:33:06 +0300

    Reworked models product

--- games_shop/catalog/models.py
+++ games_shop/catalog/models.py
@@ -6,12 +6,16 @@ class Category(models.Model):
     description = models.TextField()
     slug = models.SlugField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.IntegerField()
+    price = models.FloatField()
+    image = models.ImageField(null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name="Category", null=True)
-    date_release = models.DateTimeField()
-    date_publish = models.DateTimeField(auto_now=True)
+    date_release = models.DateField()
+    date_publish = models.DateField(auto_now=True)
     slug = models.SlugField(max_length=255, unique=True)
