0cd0941ca855fdcb57eacc955d00127fb0a501c9
LeitAnton <anton_lobanov02@mail.ru>
2022-10-22 09:03:30 +0300

    Connected postgresql

--- games_shop/games_shop/settings.py
+++ games_shop/games_shop/settings.py
@@ -76,11 +76,16 @@ WSGI_APPLICATION = 'games_shop.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres',
+       'USER': 'lonton',
+       'PASSWORD': '12345',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
+
 
 
 # Password validation
@@ -118,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
