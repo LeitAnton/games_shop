5c2b071686b1f9cbfd76387f22aebc61345ec55f
LeitAnton <anton_lobanov02@mail.ru>
2022-10-23 16:38:06 +0300

    Create a registration. Add favicon

--- games_shop/users/views.py
+++ games_shop/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import CreationForm
+from .forms import UserRegistrationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
@@ -8,8 +8,17 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class SignUp(CreateView):
-    model = User
-    form_class = CreationForm
-    success_url = reverse_lazy("signup")
-    template_name = "signup.html"
+def SignUp(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request, 'catalog.html')
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'signup.html', {'user_form': user_form})
